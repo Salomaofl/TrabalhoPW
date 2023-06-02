242e7b1359c3781f8139021c2ce51970af431389
SalomaoFlima <100690807+Salomaofl@users.noreply.github.com>
2023-06-01 23:42:27 -0300

    Contato
    Criei a funcao contato para chamar onde esta o link no header

--- src/Componentes/header.js
+++ src/Componentes/header.js
@@ -2,9 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
+//Importando os cards para a pagina principal
 import C1 from './Card1';
 import C2 from './Card2';
 import C3 from './Card3';
+import Contato from '../Paginas/contato';
 
 
 function Header(){
@@ -15,7 +18,7 @@ return(
     <div>
     <Navbar bg="dark" variant="dark">
         <Container>
-            <Navbar.Brand  href="#home"> H.OLHOS</Navbar.Brand>
+            <Navbar.Brand  href="#home"><img src="Holhos.jpeg" width={90}></img>  H.OLHOS</Navbar.Brand>
             <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#contato">Sobre Nós</Nav.Link>
@@ -26,6 +29,8 @@ return(
      < C1/>
      <C2/>
      <C3/>
+     <Contato/> 
+
     </div>
     )
 }
--- src/Paginas/contato.js
+++ src/Paginas/contato.js
@@ -1,9 +1,25 @@
-
 function Contato(){
 
 return(
-
-    <h1>Contato</h1>
+<>
+<div class="card text-center">
+  <div class="card-header">
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">CONTATO</h5>
+    <p class="card-text">E-mail:
+    Telefone:
+    Ramal:
+    
+    
+    </p>
+    <a href="#" class="btn btn-primary">Volte ao inicio</a>
+  </div>
+  <div class="card-footer text-muted">
+    Grupo 11
+  </div>
+</div>
+</>
 )
 
 
